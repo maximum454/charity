c7e4fb059734c89880355b6c0988c71fe9ea3afa
maxim <maximum454@gmail.com>
2018-09-03 16:39:21 +0600

    fix

--- src/js/charity.js
+++ src/js/charity.js
@@ -1,6 +1,11 @@
 //= partials/jquery.min.js
 //= partials/jquery.arcticmodal-0.3.min.js
 $(function () {
+    /*link*/
+    $("a[href='#']").click(function () {
+        return false;
+    });
+
     $('.popap_box').click(function () {
         $('#modalbox_' + $(this).data('body')).arcticmodal({
             overlay: {
